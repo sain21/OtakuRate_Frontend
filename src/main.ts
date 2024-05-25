da24746337669ddf3df182243c510e694fd5494f
sain21 <sainkinteh@outlook.de>
2024-05-26 01:08:47 +0200

    Frontend mit Backend render

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
-import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
-import App from './App.vue';
-import ForumView from './views/ForumView.vue';
+import './assets/main.css'
 
-const routes = [
-  { path: '/forum', component: ForumView }
-];
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
+const app = createApp(App)
 
-createApp(App).use(router).mount('#app');
+app.use(router)
+
+app.mount('#app')
