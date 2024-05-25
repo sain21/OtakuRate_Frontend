ff75ad7db907a05d131dcb4057bfa7d789b56484
sain21 <sainkinteh@outlook.de>
2024-05-25 15:14:16 +0200

    shims-vue.d.ts. hinzugefügt

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import ForumView from '../views/ForumView.vue' // Importieren Sie die ForumView
+import ForumView from '../views/ForumView.vue'
+// Importieren Sie die ForumView
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
