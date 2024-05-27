da7f8f3981c2dd8c32fe1ab59da1737adbc39a84
sain21 <sainkinteh@outlook.de>
2024-05-27 21:21:20 +0200

    M3

--- src/components/RatingListComponent.vue
+++ src/components/RatingListComponent.vue
@@ -1,20 +1,86 @@
-<script setup lang="ts">
-import { defineProps } from 'vue'
-import type { Post } from '@/model/model';
-
-const props = defineProps({
-  modelValue: Array as () => Post[]
-})
-
-</script>
-
 <template>
-  <div v-for="post in modelValue" :key="post.anime">
-    <div>Anime: {{ post.anime }}</div>
-    <div>Rating: {{ post.rating }}</div>
-    <div>Opinion: {{ post.opinion }}</div>
+  <h3> {{ title }} </h3>
+  <div>
+    <input v-model="animeField" placeholder="Anime Name" type="text">
+    <input v-model="ratingField" placeholder="Rating" type="number" @keyup.enter="save()">
+    <input v-model="opinionField" placeholder="Your Opinion" type="text" @keyup.enter="save()">
+    <button type="button" @click="save()">Save</button>
+  </div>
+  <div>
+    <table>
+      <thead>
+      <tr>
+        <th>Anime</th>
+        <th>Rating</th>
+        <th>Opinion</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-if="posts.length === 0">
+        <td colspan="3">No ratings yet</td>
+      </tr>
+      <tr v-for="post in posts" :key="post.anime">
+        <td>{{post.anime}}</td>
+        <td>{{post.rating}}</td>
+        <td>{{post.opinion}}</td>
+      </tr>
+      <tr>
+        <td>{{ animeField }}</td>
+        <td>{{ ratingField }}</td>
+        <td>{{ opinionField }}</td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+
+
+<script setup lang="ts">
+import {ref, onMounted} from 'vue'
+import axios from 'axios'
+import type {AxiosResponse} from 'axios'
+import type {Post} from '@/model/model';
+import type {Ref} from 'vue'
+
+
+
+defineProps<{
+  title: string
+}>()
+const posts: Ref<Post[]> = ref([])
+const animeField = ref('')
+const ratingField = ref(0)
+const opinionField = ref('')
+
+const $auth = useAuth()
+const email = ref('')
+
+async function loadThings (owner: string = '') {
+  const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL // 'http://localhost:8080' in dev mode
+  const endpoint = baseUrl + '/rate' + '?owner=' + owner
+  const response: AxiosResponse = await axios.get(endpoint);
+  const responseData: Post[] = response.data;
+  responseData.forEach((post: Post) => {
+    posts.value.push(post)
+  })
+}
+
+async function save () {
+  const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL // 'http://localhost:8080' in dev mode
+  const endpoint = baseUrl + '/rate'
+  const data: Post = {
+    anime: animeField.value,
+    rating: ratingField.value,
+    opinion: opinionField.value,
+  }
+  const response: AxiosResponse = await axios.post(endpoint, data);
+  const responseData: Post = response.data;
+  console.log('Success:', responseData)
+}
+</script>
+
+
 
 <style scoped>
 
