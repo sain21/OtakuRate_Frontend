2fd49da57d7cb7ded796e7d93e62df1628b44354
sain21 <sainkinteh@outlook.de>
2024-05-27 20:22:29 +0200

    M3

--- src/components/RatingListComponent.vue
+++ src/components/RatingListComponent.vue
@@ -1,10 +1,46 @@
 <script setup lang="ts">
 import { defineProps } from 'vue'
 import type { Post } from '@/model/model';
+import { ref } from 'vue';
+import axios from 'axios';
+import type { AxiosResponse } from 'axios';
+
+let posts = ref<Post[]>([]);
+let newAnime = ref('');
+let newRating = ref(0);
+let newOpinion = ref('');
+
+const addNewPost = () => {
+  let newPost: Post = { anime: newAnime.value, rating: newRating.value, opinion: newOpinion.value };
+  posts.value.push(newPost);
+  newAnime.value = '';
+  newRating.value = 0;
+  newOpinion.value = '';
+}
+
+
+
 
 const props = defineProps({
   modelValue: Array as () => Post[]
 })
+const loadThings = async (owner: string = '') => {
+  const baseURL = import.meta.env.VITE_BACKEND_URL
+  const endpoint = baseURL + '/posts';
+
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow' as RequestRedirect,
+  }
+  fetch(endpoint, requestOptions)
+    .then(response => response.json())
+    .then((result: Post[]) => result.forEach(post => {
+      posts.value.push(post)
+    }))
+    .catch(error => console.log('error', error));
+}
+
+loadThings();
 
 </script>
 
@@ -13,6 +49,18 @@ const props = defineProps({
     <div>Anime: {{ post.anime }}</div>
     <div>Rating: {{ post.rating }}</div>
     <div>Opinion: {{ post.opinion }}</div>
+  </div>
+  <h1>Anime Übersicht</h1>
+  <RatingListComponent v-model="posts"></RatingListComponent>
+
+  <div>
+    <label>Anime Name</label>
+    <input type="text" v-model="newAnime">
+    <label>Anime Rating</label>
+    <input type="number" v-model="newRating">
+    <label>Your Opinion</label>
+    <input type="text" v-model="newOpinion">
+    <button @click="addNewPost">Hinzufügen</button>
   </div>
 </template>
 
--- src/views/RatingOverview.vue
+++ src/views/RatingOverview.vue
@@ -1,52 +1,9 @@
 <script setup lang="ts">
-import type { Post } from '@/model/model';
-import { ref } from 'vue';
-import axios from 'axios';
-import type { AxiosResponse } from 'axios';
 import RatingListComponent from '@/components/RatingListComponent.vue';
-
-let posts = ref<Post[]>([]);
-let newAnime = ref('');
-let newRating = ref(0);
-let newOpinion = ref('');
-
-const addNewPost = () => {
-  let newPost: Post = { anime: newAnime.value, rating: newRating.value, opinion: newOpinion.value };
-  posts.value.push(newPost);
-  newAnime.value = '';
-  newRating.value = 0;
-  newOpinion.value = '';
-}
-
-console.log("Backend URL: ", import.meta.env.VITE_BACKEND_URL);
-
-const loadThings = async (owner: string = '') => {
-  const baseUrl = import.meta.env.VITE_BACKEND_URL
-  const endpoint = baseUrl + '/rate' + '?owner=' + owner
-  const response: AxiosResponse = await axios.get(endpoint);
-  const responseData: Post[] = response.data;
-  responseData.forEach((post: Post) => {
-    posts.value.push(post)
-  });
-}
-
-loadThings();
-
+import { RouterLink } from 'vue-router';
 </script>
 <template>
-  <h1>Anime Übersicht</h1>
-  <RatingListComponent v-model="posts"></RatingListComponent>
-
-  <div>
-    <label>Anime Name</label>
-    <input type="text" v-model="newAnime">
-    <label>Anime Rating</label>
-    <input type="number" v-model="newRating">
-    <label>Your Opinion</label>
-    <input type="text" v-model="newOpinion">
-    <button @click="addNewPost">Hinzufügen</button>
-  </div>
-
+  <RatingListComponent title = "Welcome" />
 </template>
 
 <style scoped>
@@ -54,11 +11,8 @@ h3 {
   margin-bottom: 30px;
 }
 
-
 h1 {
   width: 300%;
   margin: auto;
 }
-
-
 </style>
