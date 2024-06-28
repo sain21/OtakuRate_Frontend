642fd29f418cba7941669fec5179643e220a78fa
sain21 <sainkinteh@outlook.de>
2024-06-28 20:13:16 +0200

    login ghinzugefügt

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import { auth } from '../firebase'
+import LoginView from '../views/LoginView.vue'
 
 const routes = [
   {
